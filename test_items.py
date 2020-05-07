0d17663e7ae63e6c5ab54e29f132e5e7bcfbdd66
ivanbolsakov <Volvofh12380>
2020-05-07 22:00:08 +0300

    new case for test lesson

--- test_items.py
+++ test_items.py
@@ -1,1 +1,7 @@
+link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
 
+def test_quest_should_see_button(browser):
+    browser.get(link)
+    button = browser.find_element_by_css_selector('.btn-add-to-basket')
+    assert button.text == 'Ajouter au panier', \
+        'not found button'
