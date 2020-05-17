4f469d37bd70574032cdc2271515db4eb7d5b7ac
ivanbolsakov <Volvofh12380>
2020-05-17 20:01:22 +0300

    1.1.1

--- test_items.py
+++ test_items.py
@@ -3,6 +3,6 @@ link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
 
 def test_quest_should_see_button(browser):
     browser.get(link)
-    browser.implicitly_wait(10)
-    add_button=browser.find_elements_by_css_selector("#add_to_basket_form1")
+    browser.implicitly_wait(5)
+    add_button=browser.find_elements_by_css_selector("#add_to_basket_form")
     assert len(add_button)>0, "button not found"
